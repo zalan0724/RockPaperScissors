f933b480fdf7e3c210f3f18939773980a6f69748
zalan0724 <munkacsi0724@gmail.com>
2021-01-27 21:20:01 +0100

    GUI added

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
+// 0 = draw, 1 = win, 2 = lose
+let winTable = [[0,1,2], [2,0,1], [1,2,0]];
+let items = ['Rock', 'Paper', 'Scissors'];
+let playerPoints = 0;
+let computerPoints = 0;
+
 function computerPlay(){
     return Math.floor(Math.random()*3);
 }
-function playerPlay(){
-    let input = prompt('Rock, Paper or Scissors?');
-    let playerChoice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase(); 
-    switch(playerChoice){
-        case 'Rock':
-            return 0;
-        case 'Paper':
-            return 1;
-        case 'Scissors':
-            return 2;
-    }
-}
- function playRound(computerChoice, playerChoice){
-    // 0 = draw, 1 = win, 2 = lose
-    let winTable = [[0,1,2], [2,0,1], [1,2,0]];
-    let result = winTable[parseInt(computerChoice)][parseInt(playerChoice)];
-    let items = ['Rock', 'Paper', 'Scissors'];
 
+function playRound(playerChoice, computerChoice){
+    
+    switch(playerChoice){
+        case 0:
+            document.getElementById('playerRock').style.display = 'inline';
+            break;
+        case 1:
+            document.getElementById('playerPaper').style.display = 'inline';
+            break;
+        case 2:
+            document.getElementById('playerScissors').style.display = 'inline';
+            break;
+    }
+    switch(computerChoice){
+        case 0:
+            document.getElementById('computerRock').style.display = 'inline';
+            break;
+        case 1:
+            document.getElementById('computerPaper').style.display = 'inline';
+            break;
+        case 2:
+            document.getElementById('computerScissors').style.display = 'inline';
+            break;
+    }
+
+    let result = winTable[computerChoice][playerChoice];
     switch(result){
         case 0:
-            console.log('It\'s draw!');
-            return '00';
+            document.getElementById('winOrNot').innerHTML = 'It\'s draw!';
+            document.getElementById('details').innerHTML = 'Nobody wons!';
+            updateScore();
+            break;
         case 1:
-            console.log(`You win! ${items[playerChoice]} beats ${items[computerChoice]}`);
-            return '10';
+            document.getElementById('winOrNot').innerHTML = 'You won!';
+            document.getElementById('details').innerHTML = `${items[playerChoice]} beats ${items[computerChoice]}!`;
+            playerPoints++;
+            updateScore();
+            break;
         case 2:
-            console.log(`You Lose! ${items[computerChoice]} beats ${items[playerChoice]}`);
-            return '01';
+            document.getElementById('winOrNot').innerHTML = 'You lost!';
+            document.getElementById('details').innerHTML = `${items[computerChoice]} beats ${items[playerChoice]}!`;
+            computerPoints++;
+            updateScore();
+            break;
     }
- }
+}
+function updateScore(){
+    document.getElementById('scores').innerHTML = `${playerPoints} : ${computerPoints}`;
+}
+function reset(){
+    document.getElementById('playerRock').style.display = 'none';
+    document.getElementById('playerPaper').style.display = 'none';
+    document.getElementById('playerScissors').style.display = 'none';
+    document.getElementById('computerRock').style.display = 'none';
+    document.getElementById('computerPaper').style.display = 'none';
+    document.getElementById('computerScissors').style.display = 'none';
+}
 
- function playGame(){
-    alert('It\'s a console game!');
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for(let i=0; i<5; i++){
-        let points = playRound(computerPlay(), playerPlay());
-        playerPoints += parseInt(points.charAt(0));
-        computerPoints += parseInt(points.charAt(1));
-        console.log(playerPoints + ' : ' + computerPoints);
-     }
-    if(playerPoints>computerPoints) console.log('You won the game!');
-    else console.log('You lost the game!');
- }
+function buttonClick(value){
+    reset();
+    playRound(value, computerPlay());
+}
